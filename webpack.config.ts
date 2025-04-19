7f97f76631dccf5177704d45b5f6b1217aefa0d6
noname0310 <hjnam2014@gmail.com>
2025-04-20 02:11:24 +0900

    update deps

--- webpack.config.ts
+++ webpack.config.ts
@@ -18,11 +18,11 @@ export default (env: any): webpack.Configuration & { devServer?: WebpackDevServe
             chunks: "all",
             cacheGroups: {
                 glslShaders: {
-                    test: (module: { type: string; resource: string | undefined }): boolean => {
-                        if (module.resource === undefined) {
+                    test: (module: webpack.Module): boolean => {
+                        if ((module as webpack.NormalModule).resource === undefined) {
                             return false;
                         }
-                        const resource = module.resource.replace(/\\/g, "/");
+                        const resource = (module as webpack.NormalModule).resource.replace(/\\/g, "/");
                         if (resource.includes("Shaders/")) {
                             return true;
                         }
@@ -33,11 +33,11 @@ export default (env: any): webpack.Configuration & { devServer?: WebpackDevServe
                     enforce: true
                 },
                 wgslShaders: {
-                    test: (module: { type: string; resource: string | undefined }): boolean => {
-                        if (module.resource === undefined) {
+                    test: (module: webpack.Module): boolean => {
+                        if ((module as webpack.NormalModule).resource === undefined) {
                             return false;
                         }
-                        const resource = module.resource.replace(/\\/g, "/");
+                        const resource = (module as webpack.NormalModule).resource.replace(/\\/g, "/");
                         if (resource.includes("ShadersWGSL/")) {
                             return true;
                         }
